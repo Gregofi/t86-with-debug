176f41d8d6c14530651a467cabdbe23b81ff1113
Filip Gregor <filip.gregor98@gmail.com>
2023-02-19 14:22:00 +0100

    Add basic breakpoint handling to debug class

--- t86/t86/debug.h
+++ t86/t86/debug.h
@@ -19,12 +19,47 @@ public:
     /// Opens connection at specified port and
     /// waits for debugger to connect.
     /// This call is blocking.
-    bool OpenConnection();
+    void OpenConnection() {
+        server.Initialize();
+    }
+
+    std::string ReasonToString(BreakReason reason) {
+        switch (reason) {
+            case BreakReason::SoftwareBreakpoint: return "Software breakpoint";
+            case BreakReason::HardwareBreakpoint: return "Hardware breakpoint";
+            case BreakReason::SingleStep: return "Single step";
+            case BreakReason::Halt: return "Halt";
+        }
+    }
 
     /// Use to pass control to the debug interface
     /// which will communicate with the client.
     /// Should be called on any break situation.
-    bool Work(BreakReason reason);
+    bool Work(BreakReason reason) {
+        server.Send("Program stopped");
+        bool end = false;
+        while (end) {
+            auto message = server.Receive();
+            // TODO: This means that we received EOF, which shouldn't
+            //       really happen.
+            if (!message) {
+                return false;
+            }
+            
+            if (message == "REASON") {
+                std::string r = ReasonToString(reason);
+                server.Send(r);
+            } else if (message == "CONTINUE") {
+                end = true;
+            } else {
+                server.Send("Unknown command");
+                continue;
+            }
+            server.Send("Ok");
+        }
+
+        return true;
+    }
 private:
     Cpu& cpu;
     TCP::TCPServer server;
