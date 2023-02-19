10173f1624169ff51ed925aeb044adde44a3dcff
Filip Gregor <filip.gregor98@gmail.com>
2023-02-19 23:36:12 +0100

    WIP: make dbg interactive

--- t86/debugger/main.cpp
+++ t86/debugger/main.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include "common/TCP.h"
 #include "common/logger.h"
 
@@ -5,13 +7,17 @@ int main() {
     TCP::TCPClient c(9110);
     c.Initialize();
     log_info("Connected to VM");
-
-    auto s = c.Receive();
-    std::cout << *s << std::endl;
-    c.Send("REASON");
-    s = c.Receive();
-    std::cout << *s << std::endl;
-    c.Send("CONT");
-    s = c.Receive();
-    std::cout << *s << std::endl;
+    while (true) {
+        auto s = c.Receive();
+        std::cout << *s << std::endl;
+        std::string command;
+        std::getline(std::cin, command);
+        if (!std::cin) {
+            break;
+        }
+        c.Send(command);
+        if (command == "CONTINUE") {
+            c.Receive();
+        }
+    }
 }
