b397126e53fec0baed8c4f4d2d352a1390e9f63c
Filip Gregor <filip.gregor98@gmail.com>
2023-02-24 12:40:31 +0100

    Fix fmt::print not behaving as expected in c++20 with consteval and forward arguments to it

--- t86/common/logger.h
+++ t86/common/logger.h
@@ -2,42 +2,41 @@
 
 #include <fmt/core.h>
 #include <fmt/color.h>
+#include <string_view>
 
 
 template<typename... Args>
-inline void log_debug(fmt::format_string<Args...> format_str, Args&& ...args) {
+inline void log_debug(std::string_view format_str, Args&& ...args) {
 #if LOG_LEVEL > 3
     fmt::print(stderr, fg(fmt::color::violet), "DEBUG: ");
-    fmt::print(stderr, format_str, args...);
+    fmt::print(stderr, fmt::runtime(format_str), std::forward<Args>(args)...);
     fmt::print(stderr, "\n");
 #endif
 }
 
 template<typename... Args>
-inline void log_info(fmt::format_string<Args...> format_str, Args&& ...args) {
+inline void log_info(std::string_view format_str, Args&& ...args) {
 #if LOG_LEVEL > 2
     fmt::print(stderr, fg(fmt::color::blue), "INFO: ");
-    fmt::print(stderr, format_str, args...);
+    fmt::print(stderr, fmt::runtime(format_str), std::forward<Args>(args)...);
     fmt::print(stderr, "\n");
 #endif
 }
 
 template<typename... Args>
-inline void log_warning(fmt::format_string<Args...> format_str, Args&& ...args) {
+inline void log_warning(std::string_view format_str, Args&& ...args) {
 #if LOG_LEVEL > 1
     fmt::print(stderr, fg(fmt::color::orange), "WARNING: ");
-    fmt::print(stderr, format_str, args...);
+    fmt::print(stderr, fmt::runtime(format_str), std::forward<Args>(args)...);
     fmt::print(stderr, "\n");
 #endif
 }
 
 template<typename... Args>
-inline void log_error(fmt::format_string<Args...> format_str, Args&& ...args) {
+inline void log_error(std::string_view format_str, Args&& ...args) {
 #if LOG_LEVEL > 0
     fmt::print(stderr, fg(fmt::color::crimson), "ERROR: ");
-    fmt::print(stderr, format_str, args...);
+    fmt::print(stderr, fmt::runtime(format_str), std::forward<Args>(args)...);
     fmt::print(stderr, "\n");
 #endif
 }
-
-
