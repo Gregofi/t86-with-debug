9295842f8bdd68a4dbe01b34b9d23aec1db0e993
Filip Gregor <filip.gregor98@gmail.com>
2023-02-28 21:36:59 +0100

    Fix logger constant expression

--- t86/common/logger.h
+++ t86/common/logger.h
@@ -6,37 +6,37 @@
 
 
 template<typename... Args>
-inline void log_debug(std::string_view format_str, Args&& ...args) {
+inline void log_debug(fmt::format_string<Args...> format_str, Args&& ...args) {
 #if LOG_LEVEL > 3
     fmt::print(stderr, fg(fmt::color::violet), "DEBUG: ");
-    fmt::print(stderr, fmt::runtime(format_str), std::forward<Args>(args)...);
+    fmt::print(stderr, format_str, std::forward<Args>(args)...);
     fmt::print(stderr, "\n");
 #endif
 }
 
 template<typename... Args>
-inline void log_info(std::string_view format_str, Args&& ...args) {
+inline void log_info(fmt::format_string<Args...> format_str, Args&& ...args) {
 #if LOG_LEVEL > 2
     fmt::print(stderr, fg(fmt::color::blue), "INFO: ");
-    fmt::print(stderr, fmt::runtime(format_str), std::forward<Args>(args)...);
+    fmt::print(stderr, format_str, std::forward<Args>(args)...);
     fmt::print(stderr, "\n");
 #endif
 }
 
 template<typename... Args>
-inline void log_warning(std::string_view format_str, Args&& ...args) {
+inline void log_warning(fmt::format_string<Args...> format_str, Args&& ...args) {
 #if LOG_LEVEL > 1
     fmt::print(stderr, fg(fmt::color::orange), "WARNING: ");
-    fmt::print(stderr, fmt::runtime(format_str), std::forward<Args>(args)...);
+    fmt::print(stderr, format_str, std::forward<Args>(args)...);
     fmt::print(stderr, "\n");
 #endif
 }
 
 template<typename... Args>
-inline void log_error(std::string_view format_str, Args&& ...args) {
+inline void log_error(fmt::format_string<Args...> format_str, Args&& ...args) {
 #if LOG_LEVEL > 0
     fmt::print(stderr, fg(fmt::color::crimson), "ERROR: ");
-    fmt::print(stderr, fmt::runtime(format_str), std::forward<Args>(args)...);
+    fmt::print(stderr, format_str, std::forward<Args>(args)...);
     fmt::print(stderr, "\n");
 #endif
 }
