74f634a60cc3cf66ab187747060cdedec93e6276
Filip Gregor <filip.gregor98@gmail.com>
2023-03-01 16:55:29 +0100

    Add test for ReadingText

--- t86/tests/debugger/native_test.cpp
+++ t86/tests/debugger/native_test.cpp
@@ -49,6 +49,26 @@ TEST(NativeWT86Test, Reading) {
     auto t86 = std::make_unique<T86Process>(std::move(tm2), REG_COUNT);
     Native native(std::move(t86));
     native.WaitForDebugEvent();
+
+    auto text = native.ReadText(0, 5);
+    ASSERT_EQ(text.size(), 5);
+    auto it = text.begin();
+    ASSERT_EQ(*it++, "MOV R0, 3");
+    ASSERT_EQ(*it++, "MOV R1, 2");
+    ASSERT_EQ(*it++, "ADD R0, R1");
+    ASSERT_EQ(*it++, "MOV R2, R0");
+    ASSERT_EQ(*it++, "HALT");
+
+    ASSERT_THROW({
+        native.ReadText(0, 6);
+    }, DebuggerError);
+    ASSERT_THROW({
+        native.ReadText(5, 1);
+    }, DebuggerError);
+    ASSERT_NO_THROW({
+        native.ReadText(4, 1);
+    });
+
     ASSERT_EQ(native.GetRegister("IP"), 0);
     ASSERT_EQ(native.GetRegister("R0"), 0);
     native.PerformSingleStep();
