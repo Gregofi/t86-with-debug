52f6bf89af26ed478fab3b2b164b22775af10f03
Filip Gregor <filip.gregor98@gmail.com>
2023-03-02 14:11:22 +0100

    Add tests for wrong inputs

--- t86/tests/debugger/native_test.cpp
+++ t86/tests/debugger/native_test.cpp
@@ -479,3 +479,42 @@ TEST(NativeWT86Test, PokeTextWithBreakpoints) {
     native.ContinueExecution();
     t_os.join();
 }
+
+TEST(NativeWT86Test, BreakpointsInvalid) {
+    const size_t REG_COUNT = 3;
+    ThreadQueue<std::string> q1;
+    ThreadQueue<std::string> q2;
+    auto tm1 = std::make_unique<ThreadMessenger>(q1, q2);
+    auto tm2 = std::make_unique<ThreadMessenger>(q2, q1);
+    auto program = R"(
+.text
+
+0 MOV R0, 3
+1 MOV R1, 2
+2 ADD R0, R1
+3 MOV R2, R0
+4 HALT
+)";
+    std::thread t_os(RunCPU, std::move(tm1), program, REG_COUNT);
+    auto t86 = std::make_unique<T86Process>(std::move(tm2), REG_COUNT);
+    Native native(std::move(t86));
+    native.WaitForDebugEvent();
+
+    ASSERT_THROW({native.EnableSoftwareBreakpoint(2);}, DebuggerError);
+    ASSERT_THROW({native.DisableSoftwareBreakpoint(2);}, DebuggerError);
+    ASSERT_THROW({native.UnsetBreakpoint(2);}, DebuggerError);
+    native.SetBreakpoint(2);
+    ASSERT_THROW({native.SetBreakpoint(2);}, DebuggerError);
+    native.UnsetBreakpoint(2);
+    ASSERT_THROW({native.UnsetBreakpoint(2);}, DebuggerError);
+    ASSERT_THROW({native.EnableSoftwareBreakpoint(2);}, DebuggerError);
+    ASSERT_THROW({native.DisableSoftwareBreakpoint(2);}, DebuggerError);
+    
+    native.ContinueExecution();
+    ASSERT_EQ(native.WaitForDebugEvent(), DebugEvent::ExecutionEnd);
+    ASSERT_EQ(native.GetRegister("R0"), 5);
+    ASSERT_EQ(native.GetRegister("R1"), 2);
+    ASSERT_EQ(native.GetRegister("R2"), 5);
+    native.ContinueExecution();
+    t_os.join();
+}
