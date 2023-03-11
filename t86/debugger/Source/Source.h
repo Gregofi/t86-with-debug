35b5ce0baf33d06d78fbcef3efb36c40e3f2b526
Filip Gregor <filip.gregor98@gmail.com>
2023-03-11 11:05:47 +0100

    Add mocked process.
    A class that mocks the debugged process for the purpose of testing.
    The purpose was to test the debugging location interpreter, so it
    misses implementation of several methods. They can be added
    later if needed.

--- t86/debugger/Source/Source.h
+++ t86/debugger/Source/Source.h
@@ -17,88 +17,29 @@ public:
 
     /// Sets software breakpoint at given line and returns the address
     /// of the breakpoint (the assembly address).
-    uint64_t SetSourceSoftwareBreakpoint(Native& native, size_t line) {
-        auto mapping = UnwrapOptional(line_mapping, "No debug info for line mapping");
-        auto addr =
-            UnwrapOptional(mapping.GetAddress(line),
-                           fmt::format("No debug info for line '{}'", line));
-        native.SetBreakpoint(addr);
-        return addr;
-    }
+    uint64_t SetSourceSoftwareBreakpoint(Native& native, size_t line);
 
     /// Removes software breakpoint at given line and returns the address
     /// of where the breakpoint was (the assembly address).
-    uint64_t UnsetSourceSoftwareBreakpoint(Native& native, size_t line) {
-        auto mapping = UnwrapOptional(line_mapping, "No debug info for line mapping");
-        auto addr =
-            UnwrapOptional(mapping.GetAddress(line),
-                           fmt::format("No debug info for line '{}'", line));
-        native.UnsetBreakpoint(addr);
-        return addr;
-    }
+    uint64_t UnsetSourceSoftwareBreakpoint(Native& native, size_t line);
+    uint64_t EnableSourceSoftwareBreakpoint(Native& native, size_t line);
 
-    uint64_t EnableSourceSoftwareBreakpoint(Native& native, size_t line) {
-        auto mapping = UnwrapOptional(line_mapping, "No debug info for line mapping");
-        auto addr =
-            UnwrapOptional(mapping.GetAddress(line),
-                           fmt::format("No debug info for line '{}'", line));
-        native.EnableSoftwareBreakpoint(addr);
-        return addr;
-    }
-
-    uint64_t DisableSourceSoftwareBreakpoint(Native& native, size_t line) {
-        auto mapping = UnwrapOptional(line_mapping, "No debug info for line mapping");
-        auto addr =
-            UnwrapOptional(mapping.GetAddress(line),
-                           fmt::format("No debug info for line '{}'", line));
-        native.DisableSoftwareBreakpoint(addr);
-        return addr;
-    }
+    uint64_t DisableSourceSoftwareBreakpoint(Native& native, size_t line);
 
     /// Returns latest line that corresponds to given address if 
     /// debugging information is available, otherwise returns nullopt.
-    std::optional<size_t> AddrToLine(size_t addr) {
-        if (!line_mapping) {
-            return {};
-        }
-        auto lines = line_mapping->GetLines(addr);
-        auto max = std::max_element(lines.begin(), lines.end());
-        if (max == lines.end()) {
-            return {};
-        }
-        return *max;
-    }
+    std::optional<size_t> AddrToLine(size_t addr);
 
-    std::optional<size_t> LineToAddr(size_t addr) {
-        if (!line_mapping) {
-            return {};
-        }
-        return line_mapping->GetAddress(addr);
-    }
+    std::optional<size_t> LineToAddr(size_t addr);
 
     /// Returns lines from the source file. This function does
     /// not throw if out of bounds, instead it stops.
     /// For example if the program has two lines,
     /// and idx=1, amount=3, it would return the second line only.
     /// Returns empty vector if no debugging info is provided.
-    std::vector<std::string_view> GetLines(size_t idx, size_t amount) const {
-        if (!source_file) {
-            return {};
-        }
-        std::vector<std::string_view> result;
-        for (size_t i = 0; i < amount; ++i) {
-            auto r = source_file->GetLine(i + idx);
-            if (!r) {
-                break;
-            }
-            result.emplace_back(*r);
-        }
-        return result;
-    }
+    std::vector<std::string_view> GetLines(size_t idx, size_t amount) const;
 
-    std::optional<std::string_view> GetLine(size_t line) {
-        return source_file->GetLine(line);
-    }
+    std::optional<std::string_view> GetLine(size_t line);
 private:
     template<typename T>
     static const T& UnwrapOptional(const std::optional<T>& opt, const std::string& message) {
